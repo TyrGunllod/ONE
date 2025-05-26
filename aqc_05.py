2ca1b50ef9c56ad0f76eae992605ad964a205db1
ALEX BERTOLOTI DO CARMO GRELLET <111990261+TyrGunllod@users.noreply.github.com>
2025-05-26 13:04:21 -0300

    Add files via upload

---
+++ aqc_05.py
@@ -0,0 +1,4 @@
+#5. Crie um dicionário usando o dict comprehension em que as chaves estão na lista:
+
+meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+despesa = [860, 490, 1010, 780, 900, 630, 590, 770, 620, 560, 840, 360]
